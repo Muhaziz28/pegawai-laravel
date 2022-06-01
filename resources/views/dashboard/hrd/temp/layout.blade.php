4968a75de7b2044446d6aefb758e2545ba31920c
Muhaziz28 <muhammadhaikalaziz28@gmail.com>
2022-06-01 22:50:30 +0700

    activity

--- resources/views/dashboard/hrd/temp/layout.blade.php
+++ resources/views/dashboard/hrd/temp/layout.blade.php
@@ -77,6 +77,7 @@
                 <div class="menu-inner-shadow"></div>
 
                 <ul class="menu-inner py-1">
+
                     <!-- Home -->
                     <li class="menu-header small text-uppercase"><span class="menu-header-text">Home</span></li>
                     <li class="menu-item {{ request()->is('hrd/dashboard') ? 'active' : '' }}">
@@ -99,6 +100,11 @@
                             <li class="menu-item {{ request()->is('hrd/pegawaiAktif') || request()->is('hrd/addPegawaiView') ? 'active' : '' }}">
                                 <a href="{{ route('hrd.pegawaiAktif') }}" class="menu-link">
                                     <div data-i18n="Basic">Data Pegawai Aktif</div>
+                                </a>
+                            </li>
+                            <li class="menu-item {{ request()->is('hrd/pegawaiOut') ? 'active' : '' }}">
+                                <a href="{{ route('hrd.pegawaiOut') }}" class="menu-link">
+                                    <div data-i18n="Advanced">Data Pegawai Keluar</div>
                                 </a>
                             </li>
                             <li class="menu-item {{ request()->is('hrd/pegawaiOut') ? 'active' : '' }}">
@@ -134,34 +140,7 @@
                         </ul>
                     </li>
 
-                    <li class="menu-item active open">
-                        <a href="javascript:void(0);" class="menu-link menu-toggle">
-                            <i class="menu-icon tf-icons bx bx-food-menu"></i>
-                            <div data-i18n="Invoice">Invoice</div>
-                        </a>
-                        <ul class="menu-sub">
-                            <li class="menu-item active">
-                                <a href="app-invoice-list.html" class="menu-link">
-                                    <div data-i18n="List">List</div>
-                                </a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="app-invoice-preview.html" class="menu-link">
-                                    <div data-i18n="Preview">Preview</div>
-                                </a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="app-invoice-edit.html" class="menu-link">
-                                    <div data-i18n="Edit">Edit</div>
-                                </a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="app-invoice-add.html" class="menu-link">
-                                    <div data-i18n="Add">Add</div>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
+
                     <!-- Data -->
                     <li class="menu-header small text-uppercase"><span class="menu-header-text">Data</span></li>
                     <li class="menu-item {{ request()->is('hrd/divisi') ? 'active' : ''}}">
